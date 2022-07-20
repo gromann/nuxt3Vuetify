b28fe846fe4a745af73669dfd9053313cd71a02c
gromann <alexander.grossmann95@gmail.com>
2022-07-20 14:34:28 +0200

    add pages directory

--- app.vue
+++ app.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <v-card
-        class="mx-auto"
-        width="400"
-        prepend-icon="mdi-home"
-    >
-      <template v-slot:title>
-        Hello from Vuetify!
-      </template>
-      <v-card-text>
-        When you see this inside a card, it worked!
-      </v-card-text>
-    </v-card>
-    <NuxtWelcome />
+    <nuxt-page/>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- This page correctly has only one single root element -->
+    Page content
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
